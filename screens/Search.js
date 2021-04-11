c9a6e478b62ea578d5a0da18eb2ff640c55188c0
KevinMendozaG <kevin.ymg@hotmail.com>
2021-04-10 22:18:12 -0500

    apiGooglePlaces

--- screens/Search.js
+++ screens/Search.js
@@ -1,11 +1,12 @@
 import React,{ useEffect, useState, useCallback } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import MapView from 'react-native-maps'
 import { Button } from 'react-native-elements'
 import { useFocusEffect } from '@react-navigation/native'
 
 import { getCurrentLocation } from '../utils/helpers'
 import Modal from '../components/Modal'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 
 export default function Search() {
@@ -20,15 +21,22 @@ export default function Search() {
             })()
         }, [])
     )
-    // useEffect(() => {
-    //     (async() => {
-    //         const response = await getCurrentLocation()
-    //         if(response.status){
-    //             setNewRegion(response.location)
-    //         }
-    //     })()
-    // }, [])
+    const places = async() =>{
+        const url= urlParameters(newRegion.latitude, newRegion.longitude, 1000, 'pharmacy', 'AIzaSyDbScSU4X5B5tgzQiyNFA3ROIr0k6aiYRI')
+        fetch(url).then((data) => data.json()).then((res) =>{
+            console.log(res)
+        })
+    }
 
+    const urlParameters = (latitude, longitude, radius, type, API) =>{
+        const url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+        const location= `location=${latitude},${longitude}&radius=${radius}`
+        const typeData = `&types=${type}`
+        const key = `&key=${API}`
+        const path= `${url}${location}${typeData}${key}`
+        console.log(path)
+        return path
+    }
     return (
         <View>
             <MapView
@@ -37,6 +45,10 @@ export default function Search() {
                 showsUserLocation={true}
             >
             </MapView>
+            <Button
+            title="Buscar"
+                onPress={places}
+            />
         </View>
     )
 }
